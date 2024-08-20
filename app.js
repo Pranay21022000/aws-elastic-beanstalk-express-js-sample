ccf68b3556247fa3be1540dcd1ec66609aaefee4
Pranay21022000 <155262327+Pranay21022000@users.noreply.github.com>
2024-08-20 10:19:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('aws code-pipeline for nodejs based application'));
+app.get('/', (req, res) => res.send('this is our first aws pipeline for nodejs application'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
